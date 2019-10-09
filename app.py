d93319c8937a250e1e7b97c915d9ed80eb9dfd62
Philip <pmutua@live.com>
2019-10-09 16:16:46 +0300

    chore: Add return remove print.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from webob.response import Response
 import os
 import africastalkingwrapper
 
-from africastalkingwrapper.tasks import send_message
+# from africastalkingwrapper.tasks import send_message
 
 username = os.getenv('USERNAME')   
 api_key = os.getenv('API_KEY')
@@ -25,57 +25,41 @@ sms = africastalkingwrapper.SMS
 app = API()
 
 
-
 @app.route("/api/send_sms/")
 class SendSmsHandler:
     def post(self,req, resp):
-        """
-        :param request: {"msg":"Hello Wprld", "phone_number":["+254XXXXXXXXX"]}
-        :param response:
-        :return:
-        """
+        """Send message endpoint"""
         _decoded = req.body.decode('utf-8')
         req_data = json.loads(_decoded)
-        # print(req_data)
-        # req_data = json.dumps(_decoded)
-        # print(req_data)
-        sender = "9757"
         try:
             msg = req_data["msg"]
             phone_number_list= req_data["phoneNumber"]
             phone_vals = [str(i) for i in phone_number_list]
             
-            resp.json = sms.send(msg,phone_vals,sender)
+            resp.json = sms.send(msg,phone_vals)
             return resp.json
 
         except Exception as e:
-            print(f"A problem ocuured! {e}")
+            return f"A problem occured! {e}"
 
-        # print(req_data)
-        # resp.text =print
 
-    # @app.route("/api/send_premium/")
-    # def send_premium(self,req, resp):
-    #     """
-    #     :param request:
-    #     :param response:
-    #     :return:
 
-    #     message: SMS content. REQUIRED
-    #     short_code: Your premium product shortCode. REQUIRED
-    #     recipients: An array of phone numbers. REQUIRED
-    #     keyword: Your premium product keyword.
-    #     link_id: We forward the linkId to your application when a user sends a message to your onDemand service
-    #     retry_duration_in_hours: This specifies the number of hours your subscription message should be retried in case
-    #     it's not delivered to the subscriber
-    #     """
-    #     # response = sms.send(req["msg"],req["phone_number"])
-    #     response = sms.send("Hello Message!", ["+254722212132"])
-    #     print(response)
-    #     # x = req.body.decode('utf-8')
-    #     # f = json.dumps(x)
-    #     # print(x)
-    #     # resp.text =print
+# @app.route("/api/send_premium/")
+# def post(self,request, response=None):
+#     """Send premium message endpoint"""
+#     try:
+#         print(request)
+#         _decoded = request.body.decode('utf-8')
+#         req_data = json.loads(_decoded)
+#         msg = req_data["msg"]
+#         short_code = req_data["short_code"]
+#         recipients = req_data["receipients"]
+#         resp = sms.send_premium(msg,recipients)
+   
+#         return resp.json
+#     except Exception as e:
+#         print(e)
+
 
 
     # @app.route("/api/fetch_subscriptions(/")
